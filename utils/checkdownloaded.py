6a7dfb11d1522404a732f3a5824f8199a0960113
AuroraLHT <443202293@qq.com>
2018-08-07 02:14:12 -0500

    rewrite the checker with set

--- utils/checkdownloaded.py
+++ utils/checkdownloaded.py
@@ -2,22 +2,16 @@ from pathlib import Path
 base = "https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"
 
 with open('fullurl.txt','r') as f:
-    ls = [l.rstrip() for l in f] 
+    ls = set( l.rstrip() for l in f) 
 
-zips = tuple(Path('.').glob('*.zip'))
-checkpoints = tuple(Path('.').glob('*.ari*'))
-checkpointnames = tuple( checkpoint.stem for checkpoint in checkpoints)
+zips = set( z.name for z in  Path('.').glob('*.zip'))
+checkpoints = set(cp.stem for cp in Path('.').glob('*.ari*'))
 
-for zipfile in zips:
-    if zipfile.name not in checkpointnames:
-        if "drive" not in zipfile.name:
-            rms=base + zipfile.name
-        else:
-            rms=base+zipfile.stem[:-5]+'/'+zipfile.name
-        try:
-            ls.remove(rms)
-        except Exception as e:
-            print(rms)
-            print(e)
+noneededzips = zips - checkpoints
+
+noneededzips = set(base+z for z in noneededzips) | set(base+z[:-9]+'/'+z for z in noneededzips)
+
+ls = ls - noneededzips
+
 with open('resturl.txt', 'w') as f:
-    f.write("\n".join(ls))
+    f.write("\n".join(tuple(ls)))
