d419998c9bbd59c3f348df6776ede131cab09aa0
AuroraLHT <443202293@qq.com>
2018-08-09 02:39:26 -0500

    rewrite the model.py ,complete the video plotting in dataexaminator and add a GearTest.ipynb for testing vairous models' components

--- models.py
+++ models.py
@@ -13,6 +13,18 @@ from fastai.conv_learner import *
 f = resnet34
 cut,lr_cut = model_meta[f]
 
+def meshgrid_fromHW(H, W):
+    x = torch.arange(W)
+    y = torch.arange(H)
+    return meshgrid(H, W)
+
+def meshgrid(x ,y):
+    imW = x.size(0)
+    imH = y.size(0)
+    X = x.unsqueeze(0).repeat(imH, 1)
+    Y = y.unsqueeze(1).repeat(1, imW)
+    return X, Y
+
 def get_base():
     layers = cut_model(f(True), cut)
     return nn.Sequential(*layers)
@@ -112,8 +124,6 @@ class Offset(nn.Module):
     '''
     def __init__(self):
         super().__init__()
-        #self.o= torch.zeros([1,1]).type(torch.FloatTensor)
-        #self.eye=torch.eye(3).unsqueeze(0).type(torch.FloatTensor)
         self.register_buffer('o', torch.zeros([1,1]).type(torch.FloatTensor))
         self.register_buffer('eye', torch.eye(3).type(torch.FloatTensor).unsqueeze(0))
 
@@ -134,21 +144,51 @@ class Offset(nn.Module):
         
         #print(K0.type, K2.type, K1.type, o.type, eye.type)
         angles = angles.unsqueeze(-1)
-        K = torch.cat((o, -K2, K1, K2, o, K0, -K1, K0, o), 1).view(-1, 3, 3)
-        return eye + K * angles.sin() + torch.bmm(K,K) * (1-angles.cos())
+        K = torch.cat((o, -K2, K1, K2, o, K0, -K1, K0, o), 1).view(-1, 3, 3) # form a cpro matrix
+        return eye + K * angles.sin() + torch.bmm(K,K) * (1-angles.cos()) # using the R formular
     
-    def forward(self, pose, inv_depth):
+    def forward(self, pose, inv_depth, camera):
+        """
+            Params:
+                pose: relative pose, N X 6 vectors,
+                    1-3 is the transition vector
+                    4-6 is the rotation vector
+                inv_depth: invered depth map
+                camera: intrinsic camera parameters: (fx, fy, cx, cy)
+            Return:
+                tkx: transformed camera pixel points - x-component
+                tky: transformed camera pixel points - y-component
+                dmask: binary map of pixel that keeps track in the future
+        """
+        batch, c, h, w = inv_depth.size()
+
         rot_vecs = pose[:,:3]
         tran_vecs = pose[:, 3:]
 
         rot_mats = self.rot_vec2mat(rot_vecs)
 
-        x, y = torch.meshgrid([torch.arange(height), torch.arange(width)])
-        xy = torch.stack([x, y], dim=0).expand(batch, 1, height, width)
+        # grip points preperation
+        kx, ky = V(meshgrid_fromHW(h, w)).type_as(inv_depth)
+        kx, ky = cx+0.5, cy+0.5
+        x = (kx -camera['cx']) / camera['fx']
+        y = (ky -camera['cy']) / camera['fy']
+        xy = torch.stack([x, y], dim=-1).expand(batch, height, width, 2)
         
-        offset_xyz = torch.matmul(rot_mats[:, :2].unsqueeze(1).unsqueeze(2), xy.unsqueeze(-1)).squeeze(-1) + rot_mats[:, 2:] + tran_vecs.view(*(tran_vecs.size()), 1, 1)  * inv_depth       
-        offset = offset_xyz[:, :2] / offset_xyz[:, 2]
-        return offset
+        # transformation : Output Size NX3XHXW 
+        # calculate the transformed extended homogenerous coordinate(in projective space) of the camera screen
+        homo_xyz = (torch.matmul(rot_mats[:, :, :2].unsqueeze(1).unsqueeze(2), xy.unsqueeze(-1)).squeeze(-1).permute(0,3,1,2)
+         + rot_mats[:, :, 2:].unsqueeze(-1) 
+         + tran_vecs.view(*(tran_vecs.size()), 1, 1)  * inv_depth
+        )
+        # project the pixel in "tilted" projective space to projective space       
+        xy_warp = offset_xyz[:, :2] / offset_xyz[:, 2:].clamp(min=1e-5)
+        # projective space to camera space
+        tkx = ( xy_warp[:, 0] * camera['fx'] + camera['cx'] ) #- cx.expand(batch, height, width) 
+        tky = ( xy_warp[:, 1] * camera['fy'] + camera['cy'] ) #- cy.expand(batch, heigh, width)
+
+        dmask = V(offset_xyz[:, 2:]<1e-5)
+
+        return tkx, tky, dmask
 
 class BilinearProj(nn.Module):
     """
@@ -157,20 +197,30 @@ class BilinearProj(nn.Module):
     """
     def __init__(self):
         super().__init__()
-        self.padding = 1
-        self.pad = nn.ReflectionPad2d(self.padding)
+        #self.padding = 1
+        #self.pad = nn.ReflectionPad2d(self.padding)
 
-    def forward(self, imgs, offset):
-        # offset in pixel level
+    def forward(self, imgs, kx, ky):
+        """
+            Param:
+                imgs : batch of images in Variable Type
+                kx: the new location of the tranformed pixel on camera x axis 
+                ky: the new location of the tranformed pixel on camera y axis
+            Return:
+                sampled : sampled image from imgs
+                in_view_mask : binary masks show whether the pixel is out of boundary
+        """
         batch, c, height , width = imgs.size()
+
+        # n_kx stands for normalized camera points x component, range from (-1, 1)       
+        n_kx = kx/((height-1)/2) - 1
+        n_ky = ky/((width-1)/2) - 1
+        # shape of rcxy should be B X H X W X 2
+        n_kxy = torch.stack([n_kx, n_ky], dim=-1)
         
-        x, y = torch.meshgrid([torch.arange(height), torch.arange(width)])
-        x = (x + offset[:,0] + self.padding[:, 0])/(height/2) + 1
-        y = (y + offset[:,1] + self.padding[:, 1])/(width/2) + 1
-        xy = torch.stack([x, y], dim=0).expand(batch, 1, height, width)
-        
-        sampled = F.grid_sample(imgs, xy.expand(batch,3,height,width))
-        return sampled
+        sampled = F.grid_sample(imgs, n_kxy, mode='bilinear', padding_mode='border')  
+        in_view_mask = Variable(((n_kx.data > -1+2/w) & (n_kx.data < 1-2/w) & (n_ky.data > -1+2/h) & (n_ky.data < 1-2/h)).type_as(imgs.data))
+        return sampled, in_view_mask
 
 class TriAppearanceLoss(nn.Module):
     def __init__(self, scale=0.01, ws=11):
@@ -184,61 +234,21 @@ class TriAppearanceLoss(nn.Module):
 
     def forward(self, d1, d3, poses_x2, x1, x2, x3):
 
-        offset1 = self.offset.forward(pose = poses_x2[:,0], inv_depth= d1)
-        offset3 = self.offset.forward(pose = poses_x2[:,1], inv_depth= d3)
+        cx12, cy12, d_mask12,  = self.offset.forward(pose = poses_x2[:,0], inv_depth= d1)
+        cx32, cy32, d_mask32 = self.offset.forward(pose = poses_x2[:,1], inv_depth= d3)
 
-        x12 = self.sampler.forward(x1, offset1)
-        x32 = self.sampler.forward(x3, offset3)
+        x12, in_mask12 = self.sampler.forward(x1, cx12, cy12)
+        x32, in_mask32 = self.sampler.forward(x3, cx32, cy32)
 
-        ssimloss = self.SSIM(x12, x2) + self.SSIM(x32, x2)
-        l1loss = self.L1(x12, x2) + self.L1(x32, x2)
+        mask12 = (d_mask12*in_mask12)
+        mask32 = (d_mask32*in_mask32)
 
-        return ssimloss + scale * l1loss
+        x12 = mask12 * x12
+        x32 = mask32 * x32
+        x2m12 = mask12 * x2
+        x2m32 = mask32 * x2
 
-class SmoothLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.laplacian = LaplacianLayer()
-    
-    def forward(self, x):
-        return self.laplacian(x).mean()
-    
-    
-class LaplacianLayer(nn.Module):
-    def __init__(self):
-        super(LaplacianLayer, self).__init__()
-        w_nom = torch.FloatTensor([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]).view(1,1,3,3)
-        w_den = torch.FloatTensor([[0, 1, 0], [1, 4, 1], [0, 1, 0]]).view(1,1,3,3)
-        self.register_buffer('w_nom', w_nom)
-        self.register_buffer('w_den', w_den)
+        ssimloss = self.SSIM(x12, x2m12) + self.SSIM(x32, x2m32)
+        l1loss = self.L1(x12, x2m12) + self.L1(x32, x2m32)
 
-    def forward(self, input, do_normalize=True):
-        assert(input.dim() == 2 or input.dim()==3 or input.dim()==4)
-        input_size = input.size()
-        if input.dim()==4:
-            x = input.view(input_size[0]*input_size[1], 1,
-                            input_size[2], input_size[3])
-        elif input.dim()==3:
-            x = input.unsqueeze(1)
-        else:
-            x = input.unsqueeze(0).unsqueeze(0)
-        x_nom = torch.nn.functional.conv2d(input=x,
-                        weight=Variable(self.w_nom),
-                        stride=1,
-                        padding=0)
-        if do_normalize:
-            x_den = torch.nn.functional.conv2d(input=x,
-                        weight=Variable(self.w_den),
-                        stride=1,
-                        padding=0)
-            # x_den = x.std() + 1e-5
-            x = (x_nom.abs()/x_den)
-        else:
-            x = x_nom.abs()
-        if input.dim() == 4:
-            return x.view(input_size[0], input_size[1], input_size[2]-2, input_size[3]-2)
-        elif input.dim() == 3:
-            return x.squeeze(1)
-        elif input.dim() == 2:
-            return x.squeeze(0).squeeze(0)
-
+        return ssimlossAS
